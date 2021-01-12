a7ab7843775f38b81c860f23a6718301bdf397eb
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2021-01-12 13:45:38 +0000

    Move `SettingsListener` into `ServiceHandler`

--- android/src/main/kotlin/net/mullvad/mullvadvpn/service/ServiceHandler.kt
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/service/ServiceHandler.kt
@@ -5,5 +5,11 @@ import android.os.Looper
 import android.os.Message
 
 class ServiceHandler(looper: Looper) : Handler(looper) {
+    val settingsListener = SettingsListener()
+
     override fun handleMessage(message: Message) {}
+
+    fun onDestroy() {
+        settingsListener.onDestroy()
+    }
 }
