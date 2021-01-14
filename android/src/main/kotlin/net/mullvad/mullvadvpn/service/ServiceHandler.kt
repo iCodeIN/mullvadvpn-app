225ab7718df237c8800623f66d5796f3f33cb8d9
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2021-01-14 13:55:42 +0000

    Add daemon property to `ServiceHandler`

--- android/src/main/kotlin/net/mullvad/mullvadvpn/service/ServiceHandler.kt
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/service/ServiceHandler.kt
@@ -3,13 +3,20 @@ package net.mullvad.mullvadvpn.service
 import android.os.Handler
 import android.os.Looper
 import android.os.Message
+import kotlin.properties.Delegates.observable
 
 class ServiceHandler(looper: Looper) : Handler(looper) {
     val settingsListener = SettingsListener()
+
+    var daemon by observable<MullvadDaemon?>(null) { _, _, newDaemon ->
+        settingsListener.daemon = newDaemon
+    }
 
     override fun handleMessage(message: Message) {}
 
     fun onDestroy() {
         settingsListener.onDestroy()
+
+        daemon = null
     }
 }
